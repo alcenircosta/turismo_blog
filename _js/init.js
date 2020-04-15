d57d4a8e10eb0c87f074e661165ecbd32a6d7e44
Alcenir Costa <alcenir.1994@gmail.com>
2020-04-15 13:05:54 -0300

    Initial commit

---
+++ _js/init.js
@@ -0,0 +1,7 @@
+$(function($){
+	$(function(){
+		$('.button-collapse').sideNav();
+		$('.parallax').parallax();
+		alert("OI")
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
